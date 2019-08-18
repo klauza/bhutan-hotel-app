4b8c061adfe99a64aca95a361286288a15477774
klauza <klauza.uk@gmail.com>
2019-08-18 20:53:01 +0100

    css: 'not found' component

--- src/components/layout/NotFound.js
+++ src/components/layout/NotFound.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
+import NotFoundImage from '../../media/not-found.png';
 
 const NotFound = () => {
   return (
     <div>
-      page not found
+      <Row>
+        <Col xs={12} md={6} className="d-flex flex-column mx-auto justify-content-center mt-5">
+          <Image src={NotFoundImage} rounded className="col-5 d-block mx-auto"/>
+          <p className="text-center mt-3" style={{"fontSize":"2em", "fontWeight":"700"}}>Page not found</p>
+        </Col>
+      </Row>
     </div>
   )
 }
