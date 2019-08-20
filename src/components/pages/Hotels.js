07e91fd833cfeb9386949d080ea7d949e503ffc3
klauza <klauza.uk@gmail.com>
2019-08-20 15:44:01 +0100

    hotel sorting init

--- src/components/pages/Hotels.js
+++ src/components/pages/Hotels.js
@@ -1,21 +1,64 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import data from './Hotels-children/HotelsData';
 import HotelCard from './Hotels-children/HotelCard';
+import { orderBy } from "lodash";
 
 const Hotels = () => {
 
   // const [hotel, setHotel] = useState(data.hotels[0]);
-
+  const [collection, setCollection] = useState(data.hotels);
+  const [sortParams, setSortParams] = useState({direction: undefined});
+  const [selectedOption, setSelectedOption] = useState("default");
+  const [selectedOrder, setSelectedOrder] = useState("asc");
   
+
+  const handleColumnHeaderClick = (event) => {
+    document.querySelector('.blockDefault').disabled = true;
+    let sortKey = event;
+    setSelectedOption(event);
+    
+    const sortedCollection = orderBy(collection, [sortKey], [selectedOrder]);  // Sort collection  
+    
+    setCollection(sortedCollection);  //Update component state with new data
+    
+    // setSortParams({direction: sortDirection});  //Update component state with new data
+    
+  }
+
+  const handleOrder = (event) => {
+    
+    setSelectedOrder(event);
+
+    // const sortDirection = sortParams.direction === "desc" ? "asc" : "desc";  // Check, what direction now should be
+    const sortDirection = event;
+
+
+    const sortedCollection = orderBy(collection, [selectedOption], [sortDirection]);  // Sort collection 
+    setCollection(sortedCollection);  //Update component state with new data
+  
+  }
 
   return (
     <Fragment>
       <div style={searchBar}>
+        Search by: 
+        <select value={selectedOption} onChange={e => handleColumnHeaderClick(e.target.value)}>
+          <option className="blockDefault" value="default">Sort by</option>
+          <option value="price">Price</option>
+          <option value="bedrooms">Bedrooms</option>
+          <option value="bathrooms">Bathrooms</option>
+        </select>
 
+        Order
+        <select value={selectedOrder} onChange={e => handleOrder(e.target.value)}>
+          <option disabled>Order</option>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
       </div>
       
       {
-        data.hotels.map(hotel =>  <HotelCard key={hotel.id} hotel={hotel}/> )
+        collection.map(hotel =>  <HotelCard key={hotel.id} hotel={hotel}/> )
       }
      
       
