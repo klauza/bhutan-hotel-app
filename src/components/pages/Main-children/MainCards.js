ffbb22fb3c667b9bc058edb37fe7618e68e2842e
klauza <klauza.uk@gmail.com>
2019-08-22 16:15:28 +0100

    Main: styled-component apply

--- src/components/pages/Main-children/MainCards.js
+++ src/components/pages/Main-children/MainCards.js
@@ -1,8 +1,46 @@
 import React, {Fragment} from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import styled from 'styled-components';
+
+// STYLES
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  @media(max-width: 768px){
+    grid-template-columns: 1fr; 
+  }
+`;
+const Column = styled.div`
+  margin: 35px 5px;
+`;
+const Card = styled.div`
+  position: relative;
+  border: 1px solid grey;
+  width: 100%; height: auto;
+  text-align: center;
+`;
+const CardImg = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  border: 0;
+  border-radius: 50%;
+  position: absolute;
+  top: -50px; left: 50%; 
+  transform: translateX(-50%);
+`;
+const CardBody = styled.div`
+  text-align: center;
+  p:nth-child(1){ margin-top: 75px; }
+  p{ color: orange; padding: 10px 15px; }
+`;
+const Button = styled.button`
+margin-bottom: 5px;
+  padding: 5px 15px;
+  outline: 0;
+  border: 0;
+  border-radius: 3px;
+`;
+// end-styles
 
 const MainCards = () => {
   
@@ -10,71 +48,53 @@ const MainCards = () => {
     "https://images.pexels.com/photos/597909/pexels-photo-597909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "https://images.pexels.com/photos/2129814/pexels-photo-2129814.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-  ]
+  ];
 
-  const imageStyle={
-    "width": "100px",
-    "height": "100px",
-    "margin": "0 auto",
-    "border": "0",
-    "borderRadius": "50%",
-    "position": "absolute",
-    "top": "-50px", "left": "50%", "transform":"translateX(-50%)"
-  }
-  const marginTop75={
-    "marginTop": "75px"
-  }
-  // const marginTop150={
-  //   "marginTop": "150px"
-  // }
+
+
 
   return (
     <Fragment>
-      <Row>
+      <Grid>
 
-        <Col xs={12} md={4} style={marginTop75}>
-          <Card style={{ width: '100%', textAlign: "center" }}>
-            <Card.Img variant="top" src={imgs[0]} style={imageStyle} />
-            <Card.Body>
-              <Card.Title style={marginTop75}>Trust</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
+        <Column>
+          <Card>
+            <CardImg src={imgs[0]} />
+            <CardBody>
+              <p>Trust</p>
+              <p>Some quick example text to build on the card title and make up the bulk of
+                the card's content. </p>
+              <Button>button</Button>
+            </CardBody>
           </Card>
-        </Col>
+        </Column>
 
-        <Col xs={12} md={4} style={marginTop75}>
-          <Card style={{ width: '100%', textAlign: "center" }}>
-            <Card.Img variant="top" src={imgs[1]} style={imageStyle} />
-            <Card.Body>
-              <Card.Title style={marginTop75}>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
+        <Column>
+          <Card>
+            <CardImg src={imgs[1]} />
+            <CardBody>
+              <p>Trust</p>
+              <p>Some quick example text to build on the card title and make up the bulk of
+                the card's content. </p>
+              <Button>button</Button>
+            </CardBody>
           </Card>
-        </Col>
+        </Column>
 
-        <Col xs={12} md={4} style={marginTop75}>
-          <Card style={{ width: '100%', textAlign: "center" }}>
-            <Card.Img variant="top" src={imgs[2]} style={imageStyle} />
-            <Card.Body>
-              <Card.Title style={marginTop75}>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
+        <Column>
+          <Card>
+            <CardImg src={imgs[2]} />
+            <CardBody>
+              <p>Trust</p>
+              <p>Some quick example text to build on the card title and make up the bulk of
+                the card's content. </p>
+              <Button>button</Button>
+            </CardBody>
           </Card>
-        </Col>
+        </Column>
+
         
-      </Row>
+      </Grid>
     </Fragment>
   )
 }
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,18 +1,75 @@
 import React, {Fragment} from 'react';
-
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
 import MainCarrousel from './Main-children/MainCarrousel';
 import MainCards from './Main-children/MainCards';
-import Button from 'react-bootstrap/Button';
+import styled from 'styled-components';
 
+const Description = styled.div`
+  margin: 100px 0;
+  text-align: center;
+  width: 100%;
+  padding: 4rem 2rem;
+  background-color: #e9ecef;
+  display: block;
+  div{
+    
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
+const CarDiv = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  margin: 150px auto;
+`;
+const CarDesc = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const CarImage = styled.div`
+  flex: 1;
+  img{
+    width: 100%; height: 100%; object-fit: cover;
+  }
+`;
+const CarTopDesc = styled.h3`
+  text-align: center;
+`;
+const CarTitle = styled.h1`
+  text-align: center;
+  margin-top: 20px;
+  &:after{
+    content:'';
+    background: rgb(0, 0, 0);
+    width: 50%;
+    height: 3px;
+    border: 0; border-radius: 35px;
+    margin: 20px auto 20px;
+    display: block;
+  }
+`;
+const CarButton = styled.button`
+  border: 0;
+  border-radius: 4px;
+  padding: 10px;
+  background: green;
+  color: white;
+
+`;
 const Main = () => {
   return (
     <Fragment>
       <MainCarrousel/>
 
-      <Jumbotron fluid style={{"margin": "100px 0", "textAlign": "center"}}>
-        <Container>
+      <Description>
+        <div>
           <h1>By travellers for travellers</h1>
           <p>
           Once we were travellers too, just like you. Discovering new horizons, <br/>
@@ -22,28 +79,26 @@ const Main = () => {
           Now, we help you achieve even something bigger <br/>
           We help you with finding answers and provide a needed comfort. 
           </strong></p>
-        </Container>
-      </Jumbotron>
-
-      <Container>
-        <MainCards />
-      </Container>
-
-      <Container className="d-flex" style={{"margin": "150px auto"}}>
-        <div className="row">
-
-          <div className="col-md-6 col-12 d-flex flex-column align-items-center justify-content-center">
-            <h3 className="text-center m-0">Would you like to speed up your exploration?</h3>
-            <h1 className="text-center m-0 line-beneath mt-2">Rent a car!</h1>
-            <Button variant="success mb-2">Check deals</Button>
-          </div>
-
-          <div className="col-md-6 col-12 p-0">
-            <img className="img-fluid" src="https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""/>
-          </div>
-
         </div>
-      </Container>
+      </Description>
+
+      <Wrapper>
+        <MainCards />
+      </Wrapper>
+
+      <CarDiv>
+        
+          <CarDesc>
+            <CarTopDesc>Would you like to speed up your exploration?</CarTopDesc>
+            <CarTitle>Rent a car!</CarTitle>
+            <CarButton>Check deals</CarButton>
+          </CarDesc>
+
+          <CarImage>
+            <img className="img-fluid" src="https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""/>
+          </CarImage>
+       
+      </CarDiv>
 
     </Fragment>
   )
