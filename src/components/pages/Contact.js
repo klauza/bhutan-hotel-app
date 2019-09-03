96ca586532ae1120c6bb76d42aa3dd076deef1ed
klauza <klauza.uk@gmail.com>
2019-09-03 23:47:51 +0100

    Contact: header bones

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,54 +4,52 @@ import MapContact from './Contact-children/MapContact';
 import { Wrapper } from '../layout/Elements';
 
 // PAGE STYLES
-const Header = styled.h1`
-  margin-top: 100px;
-  text-align: center;
-  font-family: Verdana;
-  color: green;
-  font-weight: bold;
-  text-shadow: 1.5px 1.5px #000;
+const HeadBar = styled.div`
+  margin: 100px auto;
+  height: auto;
+  width: 70%;
+  border: 0; border-top-left-radius: 5px; border-top-right-radius: 5px;
+  background: lightcyan;
 `;
-const Paragraph = styled.p`
-  text-align: center;
-  width: 50%;
-  margin: 5px auto 100px auto; 
-  font-style: italic; font-weight: 500;
-  color: green;
-  &::after{
-    content:'';
-    display: block; width: 10%; height: 2px;
-    background: green;
-    margin: 15px auto 0;
-  }
+const HeadTitle = styled.div`
+  display: flex; flex-direction: row;
 `;
+const TitleMain = styled.h1`
+ flex: 1;
+ font-size: 3em;
+ align-self: center;
+ &::after{
+   content: '';
+   background: black;
+   height: 35px;
+   width: 3px;
+   display: inline-block;
+   margin-left: 25px;
+ }
+`;
+const TitleBody = styled.div`
+  flex: 1;
+`;
+const HeadHelp = styled.div`
+  text-align: center;
+`;
+// ---------------------- //
+
 const FlexLayout = styled.div`
-  display: flex;
+  display: flex; flex-direction: column;
   min-height: 500px;
-  margin-bottom: 150px;
+  margin-bottom: 100px;
   @media(max-width: 768px){flex-direction: column; }
+  box-shadow: 0px 5px 6px -2px rgba(117,117,117,0.65);
 `;
 const MapHeader = styled.p`
+  text-align: center;
   background: black;
   color: white;
   margin: 0;
   padding: 5px 0;
 `;
-const Left = styled.div`
-  flex: 6;
-  text-align: center;
-  box-shadow: 0px 5px 6px -2px rgba(117,117,117,0.65);
-  @media(max-width: 768px){order: 2;}
-`;
-const Right = styled.div`
-  flex: 2;
-  text-align: left;
-  align-self: center;
-  padding: 15px;
-  p{margin: 5px 0;}
-  @media(max-width: 768px){order: 1; text-align: center;}
-  
-`;
+// styles-end
 
 const Contact = () => {
 
@@ -59,26 +57,37 @@ const Contact = () => {
 
   return (
     <Wrapper>
-      <Header>CONTACT</Header>
-      <Paragraph>We can always help.<br/> We love to help.</Paragraph>
-      
-      <FlexLayout>
+      <HeadBar>
 
-        <Left>
+        <HeadTitle>
+          <TitleMain>CONTACT</TitleMain>
+          
+          <TitleBody>
+            <p>Need immediate help?</p>
+            <p>Got a question?</p>
+            <div><i className="fa fa-phone" style={{fontSize: "1.6rem"}}></i> <strong>02422515431</strong> - 24h open line</div>
+          </TitleBody>
+        </HeadTitle>
+      
+        <HeadHelp>
+          <div>
+            We can always help
+          </div>
+          <div>
+            We love to help
+          </div>
+        </HeadHelp>
+
+      </HeadBar>
+
+
+      <FlexLayout>
           <MapHeader>Trouble with finding our facility? Use our map.</MapHeader>
           <div className="map" style={{backgroundColor: "lightblue", height: "600px"}}>
 
             <MapContact/>
           
           </div>
-        </Left>
-
-        <Right>
-          <p>Need immediate help?</p>
-          <p>Got a question?</p>
-          <div><i className="fa fa-phone" style={{fontSize: "1.6rem"}}></i> <strong>02422515431</strong> - 24h open line</div>
-        </Right>
-
       </FlexLayout>
 
     </Wrapper>
