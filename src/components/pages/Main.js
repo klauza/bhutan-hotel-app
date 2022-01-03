fe45a5ec3524b291719326b3f5e6ba45b0232513
Michal Klauza <klauza.uk@gmail.com>
2022-01-03 08:21:44 +0100

    collaborators div - fix

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import MainCarrousel from './Main-children/MainCarousel';
 import MainCards from './Main-children/MainCards';
@@ -9,69 +9,86 @@ import { logo1, logo2, logo3, logo4, carImg } from '../../media/index';
 const Sponsors = styled.div`
   width: 100%;
   height: auto;
+  margin-top: 100px;
   /* background: #f2f5f7; */
   display: flex;
   flex-direction: column;
   border-bottom: 1px solid lightgrey;
-  background: rgba(255,255,255,0.35);
-  p{
+  border-top: 1px solid lightgrey;
+  background: rgba(255, 255, 255, 0.35);
+  p {
+    margin-top: 12.5px;
     text-align: center;
     letter-spacing: 3px;
     font-weight: 700;
     font-size: 1.5rem;
     text-transform: uppercase;
   }
+
+  img {
+    width: 60px;
+    height: 60px;
+    object-fit: cover;
+    margin: 0 10px;
+    transition: all 300ms ease;
+    &:hover {
+      transition: all 300ms ease;
+      transform: rotate(10deg);
+    }
+  }
 `;
 const SponsorsLogos = styled.div`
   height: 70px;
-  margin: 0 auto;
-  width: 180px;
+  margin: 12.5px auto;
+  /* width: 400px; // 180px */
   position: relative;
   overflow: hidden;
   transform: translate3d(0, 0, 0);
-
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
   @keyframes moveSlideshow {
-    100% { 
-      transform: translateX(-66.6666%);  
+    100% {
+      transform: translateX(-66.6666%);
     }
   }
 
-  img{
-    width: 60px; 
-    height: 60px; 
-    object-fit: cover; 
-    margin: 0 10px;
-  }
+  // .mover
   & > div {
     height: 70px;
-    width: 480px;
+    height: 100%;
+    width: 640px; // 480px
     position: absolute;
     top: 0;
     left: 0;
-    height: 100%;
     transform: translate3d(0, 0, 0);
+    &:hover {
+      animation-play-state: paused;
+    }
   }
+
   .mover {
     animation: moveSlideshow 4s linear infinite;
   }
 `;
 
 const Description = styled.div`
-  border-top: 1px solid lightgrey; border-bottom: 1px solid lightgrey;
+  border-top: 1px solid lightgrey;
+  border-bottom: 1px solid lightgrey;
   margin: 100px 0 150px;
   text-align: center;
   width: 100%;
   padding: 4rem 2rem;
-  background-color: rgba(255,255,255,0.35);
+  background-color: rgba(255, 255, 255, 0.35);
   display: block;
-  h1{
+  h1 {
     letter-spacing: 3px;
     font-weight: 900;
   }
-  p{
+  p {
     font-size: 1.1rem;
   }
-  p:nth-child(2){
+  p:nth-child(2) {
     margin: 20px 0;
   }
 `;
@@ -89,8 +106,10 @@ const CarDesc = styled.div`
 `;
 const CarImage = styled.div`
   flex: 1;
-  img{
-    width: 100%; height: 100%; object-fit: cover;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 `;
 const CarTopDesc = styled.h3`
@@ -99,12 +118,13 @@ const CarTopDesc = styled.h3`
 const CarTitle = styled.h1`
   text-align: center;
   margin-top: 20px;
-  &:after{
-    content:'';
+  &:after {
+    content: '';
     background: rgb(0, 0, 0);
     width: 50%;
     height: 3px;
-    border: 0; border-radius: 35px;
+    border: 0;
+    border-radius: 35px;
     margin: 20px auto 20px;
     display: block;
   }
@@ -115,59 +135,69 @@ const CarButton = styled.button`
   padding: 10px;
   background: green;
   color: white;
-  &:hover{
+  &:hover {
     cursor: pointer;
   }
 `;
 
-
 const Main = () => {
   return (
     <Fragment>
-      <MainCarrousel/>
+      <MainCarrousel />
 
       <Sponsors>
         <p>Collaborators</p>
         <SponsorsLogos>
-          <div className="mover">
-            <img src={logo1} alt=""/>
-            <img src={logo2} alt=""/>
-            <img src={logo3} alt=""/>
-            <img src={logo4} alt=""/>
-            <img src={logo1} alt=""/>
-            <img src={logo2} alt=""/>
-          </div>
+          {/* <div className="mover"> */}
+          <img src={logo1} alt="" />
+          <img src={logo2} alt="" />
+          <img src={logo3} alt="" />
+          <img src={logo4} alt="" />
+          {/* </div> */}
         </SponsorsLogos>
       </Sponsors>
-      
+
       <Description>
-          <h1>By travellers for travellers</h1>
+        <h1>By travellers for travellers</h1>
 
-          <p> <strong>Once we were travellers too, just like you. Discovering new horizons, <br/>
-          broadening knowledge, meeting new interesting people.</strong></p>
+        <p>
+          {' '}
+          <strong>
+            Once we were travellers too, just like you. Discovering new
+            horizons, <br />
+            broadening knowledge, meeting new interesting people.
+          </strong>
+        </p>
 
-          <p> Now, we help you achieve even something bigger <br/>
-          We help you with finding answers and provide a needed comfort.</p>
+        <p>
+          {' '}
+          Now, we help you achieve even something bigger <br />
+          We help you with finding answers and provide a needed comfort.
+        </p>
       </Description>
 
       <Wrapper>
         <MainCards />
-      
+
         <CarDiv>
-            <CarDesc>
-              <CarTopDesc>Would you like to speed up your exploration?</CarTopDesc>
-              <CarTitle>Rent a car!</CarTitle>
-              <Link to='/hiring'> <CarButton>Check deals</CarButton> </Link>
-            </CarDesc>
+          <CarDesc>
+            <CarTopDesc>
+              Would you like to speed up your exploration?
+            </CarTopDesc>
+            <CarTitle>Rent a car!</CarTitle>
+            <Link to="/hiring">
+              {' '}
+              <CarButton>Check deals</CarButton>{' '}
+            </Link>
+          </CarDesc>
 
-            <CarImage>
-              <img src={carImg} alt=""/>
-            </CarImage>
+          <CarImage>
+            <img src={carImg} alt="" />
+          </CarImage>
         </CarDiv>
-
       </Wrapper>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
